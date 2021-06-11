542c9cce4de8bb35c9d9f7ce4cf3ce2f41efc363
Vishesh Singh <VisheshSingh16@gmail.com>
2021-06-10 19:53:10 -0500

    dynamic pages with getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,36 @@
 import MeetupDetails from '../../components/meetups/MeetupDetails';
 
-const MeetupDetailPage = () => {
+const MeetupDetailPage = ({ meetup }) => {
   return (
     <MeetupDetails
-      title='Hillstation house'
-      image='https://wallpapercave.com/wp/wp2337103.jpg'
-      address='Wonder Garden hills, Bellford, LN'
-      description='a beautiful fort in the middle of greenlands'
+      title={meetup.title}
+      image={meetup.image}
+      address={meetup.address}
+      description={meetup.description}
     />
   );
+};
+
+export const getStaticPaths = () => {
+  return {
+    paths: [{ params: { meetupId: '1' } }, { params: { meetupId: '2' } }],
+    fallback: false,
+  };
+};
+
+export const getStaticProps = ({ params }) => {
+  const meetupId = params.meetupId;
+  return {
+    props: {
+      meetup: {
+        id: meetupId,
+        title: 'Hillstation house',
+        image: 'https://wallpapercave.com/wp/wp2337103.jpg',
+        address: 'Wonder Garden hills, Bellford, LN',
+        description: 'a beautiful fort in the middle of greenlands',
+      },
+    },
+  };
 };
 
 export default MeetupDetailPage;
